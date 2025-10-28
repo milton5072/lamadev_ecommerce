99b14d3ba146d5bfabdf9901bdc32453298908d2
milton <miltonkazi2004@gmail.com>
2025-10-28 09:37:42 +0600

    asad

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,15 +1,34 @@
 "use client";
+
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+
 const categories = [
-	{ id: 1, name: "Category 1" },
-	{ id: 2, name: "Category 2" },
-	{ id: 3, name: "Category 3" },
+	{ id: 1, name: "All", slug: "all" },
+	{ id: 10, name: "Category 1", slug: "category-1" },
+	{ id: 2, name: "Category 2", slug: "category-2" },
+	{ id: 3, name: "Category 3", slug: "category-3" },
 ];
 
 const Categories = () => {
+	const searchParams = useSearchParams();
+	const selectedCategory = searchParams.get("category");
+	const router = useRouter();
+	const pathName = usePathname;
+	const handleChange = (value: string | null) => {
+		router.push(`/?category=${value}`);
+	};
 	return (
 		<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 gap-2 bg-gray-100 p-2 rounded-md mb-4 text-sm">
 			{categories.map((category) => (
-				<div key={category.name}>{category.name}</div>
+				<div
+					className={`cursor-pointer text-center ${
+						category.slug === selectedCategory ? "bg-white" : "text-gray-500"
+					}`}
+					key={category.name}
+					onClick={() => handleChange(category.slug)}
+				>
+					{category.name}
+				</div>
 			))}
 		</div>
 	);
